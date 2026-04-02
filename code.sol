5ae5126d9b417eae4889dd7a3c36ca8991f7b5aa
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2026-04-02 23:15:04 +0300

    Update code.sol

--- code.sol
+++ code.sol
@@ -7,7 +7,7 @@ import { Constants } from "src/libraries/Constants.sol";
 // Interfaces
 import { ISemver } from "interfaces/universal/ISemver.sol";
 
-/// @custom:proxied true
+/// @custom:proxied false
 /// @custom:predeploy 0x4200000000000000000000000000000000000012
 /// @title L2Block
 /// @notice The L1 Block predeploy gives users access to information about the last known L1 block.
