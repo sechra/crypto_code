7223958657ba0e577a557b54141b4e61b81e3141
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2026-04-03 00:10:26 +0300

    Update code.sol

--- code.sol
+++ code.sol
@@ -8,7 +8,7 @@ import { Constants } from "src/libraries/Constants.sol";
 import { ISemver } from "interfaces/universal/ISemver.sol";
 
 /// @custom:proxied false
-/// @custom:predeploy 0x4200000000000000000000000000000000000012
+/// @custom:deploy 0x4200000000000000000000000000000000000012
 /// @title L2 Block
 /// @notice The L1 Block predeploy gives users access to information about the last known L1 block.
 ///         Values within this contract are updated once per epoch (every L1 block) and can only be
