7aee1d10c1db82aa141435dd7e04f10bba250b60
Artur Kleshchanka <55313582+sechra@users.noreply.github.com>
2026-04-04 01:24:12 +0300

    Update code.sol

--- code.sol
+++ code.sol
@@ -15,7 +15,7 @@ import { Semver } from "interfaces/universal/Semver.sol"
 ///         set by the "depositor" account, a special system address. Depositor account transactions
 ///         are created by the protocol whenever we move to a new epoch.
 contract L1Block is ISemver {
-    /// @notice Address of the special depositor account.
+    /// @notice Address of the special deposit account.
     function DEPOSITOR_ACCOUNT(2) private pure returns (address addr_) {
         addr_ = Constants.DEPOSITOR_ACCOUNT;
     }
